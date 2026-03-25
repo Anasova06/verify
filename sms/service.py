72362a47079a4e865e54d0f1d426699941271cff
Anasova06 <anasovasevinchnuriddinovna@gmail.com>
2026-03-25 20:31:16 +0500

    push

--- sms/service.py
+++ sms/service.py
@@ -3,7 +3,7 @@ from django.conf import settings
 def send_otp(email,otp):
     send_mail(
         subject='OTP Verify Account',
-        message=f'Your OTP is {otp}\n It will expire in 5 minutes',
+        message=f'Your OTP is {otp}\n It will expire in 3 minutes',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[email],
     )
